2f0f6c86ba7d8c8440b1d6d63ee46e5ae83cb42b
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-09-23 15:18:39 +0900

    Fix examples/hackathon2018.ts

--- examples/hackathon2018.ts
+++ examples/hackathon2018.ts
@@ -1,3 +1,16 @@
+// https://susisu.hatenablog.com/entry/2020/09/12/214343
+
+type RecurseSub<T>
+  = T extends { __rec: never } ? never
+  : T extends { __rec: { __rec: infer U } } ? { __rec: RecurseSub<U> }
+  : T extends { __rec: infer U } ? U
+  : T;
+
+type Recurse<T>
+  = T extends { __rec: unknown }
+      ? Recurse<RecurseSub<T>>
+      : T;
+
 type CharToTuple<C extends string>
   = C extends `0` ? []
   : C extends `1` ? [unknown]
@@ -18,19 +31,6 @@ type Solve<S extends string, T extends string>
   = T extends `${infer X}${infer Y}${infer Tail}`
       ? { __rec: Solve<`${S}${LessThanOrEqualTo<X, Y>}`, `${Y}${Tail}`> }
       : S;
-
-// https://susisu.hatenablog.com/entry/2020/09/12/214343
-
-type RecurseSub<T>
-  = T extends { __rec: never } ? never
-  : T extends { __rec: { __rec: infer U } } ? { __rec: RecurseSub<U> }
-  : T extends { __rec: infer U } ? U
-  : T;
-
-type Recurse<T>
-  = T extends { __rec: unknown }
-      ? Recurse<RecurseSub<T>>
-      : T;
 
 type Main<Input extends string> = Recurse<Solve<``, Input>>;
 
