714d32226e301d09a79bfb37d5aef06f4745176b
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-09-23 16:15:57 +0900

    Add tests/mayfes2018-day1.test.ts

---
+++ tests/mayfes2018-day1.test.ts
@@ -0,0 +1,17 @@
+import { run } from "../src";
+
+describe(`mayfes2018-day1`, () => {
+  test(`example 1`, async () => {
+    const { output } = await run(`examples/mayfes2018-day1.ts`, {
+      input: Buffer.from(`AAAAAAAAAAAAAAAAAAAAAAAAAA\n`),
+    });
+    expect(output.toString()).toBe(`BCDEFGHIJKLMNOPQRSTUVWXYZA`);
+  });
+
+  test(`example 2`, async () => {
+    const { output } = await run(`examples/mayfes2018-day1.ts`, {
+      input: Buffer.from(`SFBMPCVCSHDKARZHSCTVXSYGST\n`),
+    });
+    expect(output.toString()).toBe(`THEQUICKBROWNFOXJUMPSOVERT`);
+  });
+});
