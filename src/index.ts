f29739e0148453631bf304b9174187ef1660b29f
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-09-23 14:45:09 +0900

    Use promises

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as ts from 'typescript';
-import * as fs from 'fs';
-import * as tmp from 'tmp';
+import { promises as fs } from 'fs';
+import * as tmp from 'tmp-promise';
 import * as path from 'path';
 import { Readable, Writable } from 'stream';
 
@@ -15,22 +15,24 @@ export async function run(fileName: string, { stdin = process.stdin, stdout = pr
     bufferList.push(Buffer.from(chunk));
   }
   const inputBuffer = Buffer.concat(bufferList);
-  const tmpDirName = tmp.dirSync({ prefix: `compile-time-typescript` }).name;
+  const { path: tmpDirPath } = await tmp.dir({ prefix: `compile-time-typescript` });
   const calleeFileName = `callee.ts`;
   const callerFileName = `caller.ts`;
-  fs.copyFileSync(fileName, path.join(tmpDirName, calleeFileName));
-  fs.writeFileSync(path.join(tmpDirName, callerFileName), `
-    import Main from './${path.basename(calleeFileName, path.extname(calleeFileName))}';
-    type Input = ${JSON.stringify(inputBuffer.toString('binary'))};
-    type Output = Main<Input>;
-  `);
+  await Promise.all([
+    fs.copyFile(fileName, path.join(tmpDirPath, calleeFileName)),
+    fs.writeFile(path.join(tmpDirPath, callerFileName), `
+      import Main from './${path.basename(calleeFileName, path.extname(calleeFileName))}';
+      type Input = ${JSON.stringify(inputBuffer.toString('binary'))};
+      type Output = Main<Input>;
+    `),
+  ]);
   const program = ts.createProgram({
-    rootNames: [path.join(tmpDirName, callerFileName)],
+    rootNames: [path.join(tmpDirPath, callerFileName)],
     options: {
       strict: true,
     },
   });
-  const source = program.getSourceFile(path.join(tmpDirName, callerFileName))!;
+  const source = program.getSourceFile(path.join(tmpDirPath, callerFileName))!;
   const checker = program.getTypeChecker();
 
   function visit(node: ts.Node) {
@@ -48,9 +50,11 @@ export async function run(fileName: string, { stdin = process.stdin, stdout = pr
   }
 
   source.forEachChild(visit);
-  fs.unlinkSync(path.join(tmpDirName, calleeFileName));
-  fs.unlinkSync(path.join(tmpDirName, callerFileName));
-  fs.rmdirSync(tmpDirName);
+  await Promise.all([
+    fs.unlink(path.join(tmpDirPath, calleeFileName)),
+    fs.unlink(path.join(tmpDirPath, callerFileName)),
+  ]);
+  await fs.rmdir(tmpDirPath);
 }
 
 export async function main(...args: string[]) {
