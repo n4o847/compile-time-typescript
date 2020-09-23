83ef530b4609cc91751da21cac3a841430d70672
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-09-23 14:40:19 +0900

    Can specify streams

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,26 @@ import * as ts from 'typescript';
 import * as fs from 'fs';
 import * as tmp from 'tmp';
 import * as path from 'path';
+import { Readable, Writable } from 'stream';
 
-function main(...args: string[]) {
-  if (!(args.length === 1 && typeof args[0] === 'string')) {
-    throw new Error(`argument error`);
+interface RunOptions {
+  stdin: Readable;
+  stdout: Writable;
+}
+
+export async function run(fileName: string, { stdin = process.stdin, stdout = process.stdout }: Partial<RunOptions> = {}) {
+  const bufferList = [];
+  for await (const chunk of stdin) {
+    bufferList.push(Buffer.from(chunk));
   }
-  const fileName = args[0];
-  const input = fs.readFileSync(process.stdin.fd);
+  const inputBuffer = Buffer.concat(bufferList);
   const tmpDirName = tmp.dirSync({ prefix: `compile-time-typescript` }).name;
   const calleeFileName = `callee.ts`;
   const callerFileName = `caller.ts`;
   fs.copyFileSync(fileName, path.join(tmpDirName, calleeFileName));
   fs.writeFileSync(path.join(tmpDirName, callerFileName), `
     import Main from './${path.basename(calleeFileName, path.extname(calleeFileName))}';
-    type Input = ${JSON.stringify(input.toString('binary'))};
+    type Input = ${JSON.stringify(inputBuffer.toString('binary'))};
     type Output = Main<Input>;
   `);
   const program = ts.createProgram({
@@ -32,7 +38,7 @@ function main(...args: string[]) {
       const type = checker.getTypeFromTypeNode(node);
       if (node.getText() === `Main<Input>`) {
         if (type.isStringLiteral()) {
-          console.log(type.value);
+          stdout.write(type.value);
         } else {
           throw new Error(`type error: ${checker.typeToString(type)}`);
         }
@@ -47,10 +53,16 @@ function main(...args: string[]) {
   fs.rmdirSync(tmpDirName);
 }
 
-if (require.main === module) {
-  try {
-    main(...process.argv.slice(2));
-  } catch (e) {
-    console.log(e);
+export async function main(...args: string[]) {
+  if (!(args.length === 1 && typeof args[0] === 'string')) {
+    throw new Error(`argument error`);
   }
+  const fileName = args[0];
+  await run(fileName);
+}
+
+if (require.main === module) {
+  main(...process.argv.slice(2)).catch((e) => {
+    console.log(e);
+  });
 }
