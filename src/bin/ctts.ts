0b5e4c6ca042330fa7f4607fe2bcc765a6c01d01
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-12-07 00:54:19 +0900

    Add support for CLI

---
+++ src/bin/ctts.ts
@@ -0,0 +1,26 @@
+import yargs from 'yargs/yargs';
+import { run } from '..';
+
+const argv = yargs(process.argv.slice(2))
+  .usage(`Usage: $0 [options] script.ts`)
+  .version('v1.0.0')
+  .help()
+  .demandCommand(1, 1)
+  .string('_')
+  .argv;
+
+export async function main() {
+  const fileName = argv._[0];
+  const bufferList = [];
+  for await (const chunk of process.stdin) {
+    bufferList.push(Buffer.from(chunk));
+  }
+  const input = Buffer.concat(bufferList);
+  const { output } = await run(fileName, { input });
+  process.stdout.write(output.toString());
+}
+
+main().catch((e) => {
+  console.error(String(e));
+  process.exit(1);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
-const path = require('path');
 const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: {
+    'lib/index': './src/index.ts',
+    'bin/ctts': './src/bin/ctts.ts',
+  },
   output: {
-    path: path.resolve(__dirname, './lib'),
-    filename: 'index.js',
+    path: __dirname,
   },
   module: {
     rules: [
@@ -15,6 +16,9 @@ module.exports = {
         use: 'ts-loader',
       },
     ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
   },
   target: 'node',
   externals: [nodeExternals()],
