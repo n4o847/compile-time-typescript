d88c39e4358ef717e7d7a825846506047a6a1c41
n4o847 <22975590+n4o847@users.noreply.github.com>
2022-09-26 17:08:22 +0900

    Remove main function

--- src/index.ts
+++ src/index.ts
@@ -60,23 +60,3 @@ export async function run(fileName: string, { input = Buffer.of() }: Partial<Run
     output: Buffer.concat(outputList),
   };
 }
-
-export async function main(...args: string[]) {
-  if (!(args.length === 1 && typeof args[0] === 'string')) {
-    throw new Error(`argument error`);
-  }
-  const fileName = args[0];
-  const bufferList = [];
-  for await (const chunk of process.stdin) {
-    bufferList.push(Buffer.from(chunk));
-  }
-  const input = Buffer.concat(bufferList);
-  const { output } = await run(fileName, { input });
-  process.stdout.write(output.toString());
-}
-
-if (require.main === module) {
-  main(...process.argv.slice(2)).catch((e) => {
-    console.error(e);
-  });
-}
